19a05b8df777b2117d675b00cb38077c02653cef
Ainara Ramos <ainaretework@gmail.com>
2022-06-22 11:53:04 +0200

    service with methods

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,55 @@
+import { MovieList } from './../models/movie.model';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+const API_KEY = "d0a0b78d4500c347400877aac4d6a41f"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  movies: MovieList[] = [];
+
+  constructor(
+    private http: HttpClient,
+    private route: Router,
+  ) { }
+
+  getList(page: number): Observable<any> {
+    let url = `${environment.URL}movie/now_playing?language=es&api_key=${API_KEY}&page=${page}&sort_by=release_date.desc`;
+    return this.http.get<any>(url).pipe(
+      map((response) => {
+        console.log(response.results)
+        return response;
+    })
+    )
+  }
+
+  setList(myMovies: MovieList[]) {
+    this.movies = myMovies;
+  }
+
+  getDetail(id: string | null): Observable<any> {
+    let url = `${environment.URL}movie/${id}?api_key=${API_KEY}&language=es`;
+    return this.http.get<any>(url).pipe(
+      map((response) => {
+        return response;
+    })
+    )
+  }
+
+  searchMovie(page: number, query: string) {
+    let url = `${environment.URL}search/movie?api_key=${API_KEY}&language=es&query=${query}&page=${page}`;
+    return this.http.get<any>(url).pipe(
+      map((response) => {
+        return response;
+    })
+    )
+  }
+}
+
