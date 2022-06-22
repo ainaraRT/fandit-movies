5bc9ee0bc289558ae515c1c54ad2efc08eda14a9
Ainara Ramos <ainaretework@gmail.com>
2022-06-22 14:15:39 +0200

    finished detail movie

--- src/app/detail-movie/detail-movie.component.ts
+++ src/app/detail-movie/detail-movie.component.ts
@@ -13,23 +13,25 @@ export class DetailMovieComponent implements OnInit {
 
   movie!: MovieModel;
   sub: any;
-  
   id!: number;
 
   constructor(
     private movieService: MovieService,
-    private router: Router,
     private route: ActivatedRoute,
     private location: Location
   ) { }
 
   ngOnInit(): void {
     this.sub = this.route.paramMap.subscribe((parms: ParamMap) => {
+
+      //Getting the information of the movie picked
       this.movieService.getDetail(parms.get('id')).subscribe(
           response => {
             this.movie = response;
             console.log("PELICULA " + JSON.stringify(this.movie))
             if(response.poster_path != "null") {
+
+              //Getting the poster of the movie
               this.movie.poster_path = `https://image.tmdb.org/t/p/w185_and_h278_bestv2${this.movie.poster_path}`
             }
           }
@@ -37,6 +39,7 @@ export class DetailMovieComponent implements OnInit {
   }); 
   }
 
+  //Go back to the previous page
   goBack() {
     this.location.back();
   }
